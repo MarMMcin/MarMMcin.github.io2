deed96f39e707c8d097f787db0248f2849f17a35
MarMMcin <em-erce@wp.pl>
2019-09-16 13:23:48 +0200

    Updates

---
+++ precache-manifest.bfed86adb49b0f79c7304d6d81d606c5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cba15236325347736ec46b847bab4a1",
+    "url": "/MarMMcin/MarMMcin.github.io.git/index.html"
+  },
+  {
+    "revision": "09aa99a522891f121ac1",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/css/main.3e0436ac.chunk.css"
+  },
+  {
+    "revision": "2963707322b234cacbb2",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/js/2.dd097d01.chunk.js"
+  },
+  {
+    "revision": "09aa99a522891f121ac1",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/js/main.2d844db1.chunk.js"
+  },
+  {
+    "revision": "13747694842b0931cc6a",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/js/runtime~main.61964d6b.js"
+  },
+  {
+    "revision": "3707da3c6a394c7ee2523273f8d98a25",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/media/css3.3707da3c.svg"
+  },
+  {
+    "revision": "0ff1a6f08182be4aa63a0b54da1b9e57",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/media/github.0ff1a6f0.svg"
+  },
+  {
+    "revision": "f0361c5e9bebc1c5647523db385641cf",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/media/html5.f0361c5e.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "a123152868245dee71c441124a98ac3e",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/media/nodejs.a1231528.svg"
+  },
+  {
+    "revision": "4009eef3b3e291f296e71962c70895c5",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/media/react.4009eef3.svg"
+  },
+  {
+    "revision": "024196716802e272adb3cbefb81fd557",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/media/reactrouter.02419671.svg"
+  },
+  {
+    "revision": "3f5ee2d295271db65f0c48f17b34d619",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/media/rzeszow.3f5ee2d2.jpg"
+  },
+  {
+    "revision": "b446ca5a397c1ef6fb94cd02c4e94335",
+    "url": "/MarMMcin/MarMMcin.github.io.git/static/media/sass.b446ca5a.svg"
+  }
+]);
